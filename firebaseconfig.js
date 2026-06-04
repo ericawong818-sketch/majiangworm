2dba687c2a40b750b47d563787a92fb9f2eb00ee
ericawong818-sketch <ericawong818@gmail.com>
2026-06-04 13:30:24 +0800

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,1 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApNx_xG8q1uxvCPQzr7n0DPuwe23neC4c",
+  authDomain: "gathering-550ad.firebaseapp.com",
+  projectId: "gathering-550ad",
+  storageBucket: "gathering-550ad.firebasestorage.app",
+  messagingSenderId: "398133132128",
+  appId: "1:398133132128:web:6efc3733e77e9fa5e2fc05"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
